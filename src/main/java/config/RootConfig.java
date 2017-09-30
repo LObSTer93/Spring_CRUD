5f6cea7a1bcd240d3a671187af88db08a4f5f88b
LObSTer93 <LObSTer-93@mail.ru>
2017-09-30 22:57:11 +0300

    initial commit

---
+++ src/main/java/config/RootConfig.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Бизнес-конфигурация приложения
+ */
+@Configuration
+public class RootConfig {
+}
---
+++ src/main/java/config/WebAppInitalizer.java
@@ -0,0 +1,24 @@
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Главный файл конфигурации приложения
+ */
+public class WebAppInitalizer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
