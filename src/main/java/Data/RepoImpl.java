9a90a21e84e9d9e210dfe53d638dec03eb5f0585
Катышев Никита Петрович <OUT-Katyishev-NP@mail.ca.sbrf.ru>
2018-02-28 19:43:57 +0300

    getAll with h2

--- src/main/java/Data/RepoImpl.java
+++ src/main/java/Data/RepoImpl.java
@@ -1,23 +1,32 @@
 package Data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class RepoImpl implements Repo {
+
+    @Autowired
+    private JdbcOperations jdbcOperations;
+
     @Override
     public List<Info> getInfo() {
-        Info info1 = Info.builder()
-                .id(1)
-                .name("Mike")
-                .eMail("Mike@nike.com")
-        .build();
-        List<Info> infoList = new ArrayList<>();
-        infoList.add(info1);
+        return jdbcOperations.query("select id, name, email from info", new InfoRowMapper());
+    }
 
-        return infoList;
-//        return null;
+    private static final class InfoRowMapper implements RowMapper<Info> {
+        public Info mapRow(ResultSet rs, int rowNum) throws SQLException {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            String email = rs.getString("email");
+            return new Info(id, name, email);
+        }
     }
 }
--- src/main/java/config/RootConfig.java
+++ src/main/java/config/RootConfig.java
@@ -4,10 +4,12 @@ import Data.Repo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Бизнес-конфигурация приложения
@@ -19,10 +21,21 @@ public class RootConfig {
     @Bean
     public DataSource dataSource(){
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+
         return builder.
                 setType(EmbeddedDatabaseType.H2).
                 addScript("classpath:schema.sql").
                 addScript("classpath:data.sql").
                 build();
+    }
+
+    @Bean(initMethod="start",destroyMethod="stop")
+    public org.h2.tools.Server h2WebConsonleServer () throws SQLException {
+        return org.h2.tools.Server.createWebServer("-web","-webAllowOthers","-webDaemon","-webPort", "8082");
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
